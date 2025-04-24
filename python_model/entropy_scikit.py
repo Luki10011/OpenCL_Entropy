68c550d556a55dfd46b670d514461fd5a48a8ab6
romannowak9 <roman.nowak9@interia.pl>
2025-04-24 09:47:35 +0200

    scikit times

--- python_model/entropy_scikit.py
+++ python_model/entropy_scikit.py
@@ -3,31 +3,36 @@ from skimage.morphology import disk
 import cv2
 import os
 import numpy as np
+import time
+
+
+IMG_DIR = "test_images"
+OUT_DIR = "out"
+
+
+def test_entropy(img_name: str, struct_elem: np.ndarray) -> float:
+    img = cv2.imread(os.path.join(IMG_DIR, img_name), 0)
+    
+    start = time.time()
+    entr_img = cv2.normalize(entropy(img, struct_elem), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX).astype(np.uint8)
+    comp_time = time.time() - start
+    print(f"Elapsed time for image {img_name}: {comp_time} s")
+    
+    if not os.path.exists(OUT_DIR):
+        os.mkdir(OUT_DIR)
+        
+    cv2.imwrite(os.path.join(OUT_DIR, "entr_img.png"), entr_img)
+    
+    return comp_time
 
 
 def main():
-    images_dir = "test_images"
-    out_dir = "out"
     struct_elem = disk(5)
     
-    cameraman = cv2.imread(os.path.join(images_dir, "cameraman.png"), 0)
-    scikit_cameraman = cv2.imread(os.path.join(images_dir, "scikit_cameraman.png"), 0)
-    circuit = cv2.imread(os.path.join(images_dir, "circuit.png"), 0)
-    peppers = cv2.imread(os.path.join(images_dir, "peppers.png"), 0)
-    
-    entr_cameraman = cv2.normalize(entropy(cameraman, struct_elem), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX).astype(np.uint8)
-    entr_scikit_cameraman = cv2.normalize(entropy(scikit_cameraman, struct_elem), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX).astype(np.uint8)
-    entr_circuit = cv2.normalize(entropy(circuit, struct_elem), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX).astype(np.uint8)
-    entr_peppers = cv2.normalize(entropy(peppers, struct_elem), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX).astype(np.uint8)
-    
-    if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
-        
-    cv2.imwrite(os.path.join(out_dir, "entr_cameraman.png"), entr_cameraman)
-    cv2.imwrite(os.path.join(out_dir, "entr_scikit_cameraman.png"), entr_scikit_cameraman)
-    cv2.imwrite(os.path.join(out_dir, "entr_circuit.png"), entr_circuit)
-    cv2.imwrite(os.path.join(out_dir, "entr_peppers.png"), entr_peppers)
-    
+    test_entropy("cameraman.png", struct_elem)
+    test_entropy("scikit_cameraman.png", struct_elem)
+    test_entropy("circuit.png", struct_elem)
+    test_entropy("peppers.png", struct_elem)
     
 if __name__ == "__main__":
     main()
