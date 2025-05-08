3cf0bfb415fcb630b31729141a915c917e5b93cc
Roman Nowak <roman.nowak9@interia.pl>
2025-05-08 11:49:20 +0200

    new_cmake

--- ImageEntropy/include/opencl_utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <CL/cl.h>
-
-void init_opencl();
-void cleanup_opencl();
--- ImageEntropy/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "opencl_utils.h"
-
-int main() {
-    std::cout << "OpenCL Entropy Calculator\n";
-    return 0;
-}
